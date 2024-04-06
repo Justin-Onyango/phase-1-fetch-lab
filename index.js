6bc31cb7eacea1afdb4469c6b9075a1726b83b2e
Justin Onyango <ojustin446@gmail.com>
2024-04-06 13:01:34 +0300

    Lab

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => response.json())
   
 }
 
-function renderBooks(books) {
+function renderBooks() {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
